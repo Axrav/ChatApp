67539df54b12d8d411cb2435a2c9c32c6dfcc304
Aarav Arora <aaravarora.works@gmail.com>
2022-10-19 03:28:02 +0530

    feat: logger

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"chatapp/controllers"
 	"chatapp/router"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := router.Setup()
+	app.Use(logger.New())
 	go controllers.Init.Background()
 	//go Init.Background()
 	app.Listen(":9898")
